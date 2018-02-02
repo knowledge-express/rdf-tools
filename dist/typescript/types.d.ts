d416c82bc1ffae424ef0aa4202a29e0103abadfb
Steffan Sluis <steffansluis@gmail.com>
2018-02-02 02:19:32 +0100

    Add RDF.Resource, extend and alias

--- dist/typescript/types.d.ts
+++ dist/typescript/types.d.ts
@@ -2,6 +2,11 @@ import { Property, Class } from '../model';
 export declare function nativeTypesToTS(): string;
 export declare function propertyToTS(propertyObj: Property): string;
 export declare function classToTS(classObj: Class, classIris: string[]): string;
+export declare function rdfResourceToTS(): string;
+export declare function classToTSAliases(classObj: Class, classIris: string[]): string[];
+export declare function aliasRDFResources(classesObj: {
+    classes: Class[];
+}): string;
 export declare function classesToTS(classesObj: {
     classes: Class[];
 }): string;
